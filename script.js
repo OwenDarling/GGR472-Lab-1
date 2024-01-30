a450b9e96e6a8690b1096dc6eed7626874ba0afe
OwenDarling <156254189+OwenDarling@users.noreply.github.com>
2024-01-30 10:25:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ const btn = document.getElementById("help-button");
 btn.addEventListener("click", function(){
     alert("Please click one of the links to be directed to either google or the GeoJSON code.");
 })
-let numyes = 0;
-const btn = document.getElementById("yesbtn");
-btn.addEventListener("click",function(){
-    numyes += 1 ;
-})
